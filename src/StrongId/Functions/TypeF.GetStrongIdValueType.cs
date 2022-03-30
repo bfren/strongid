1e8eb388f9e8d270ab83627ce936125cd1750c2a
Ben Green <ben@bcgdesign.com>
2022-03-30 08:19:56 +0100

    Adding StrongId functions - #1

---
+++ src/StrongId/Functions/TypeF.GetStrongIdValueType.cs
@@ -0,0 +1,45 @@
+// Mileage Tracker
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
+
+using Microsoft.Extensions.Caching.Memory;
+
+namespace StrongId.Functions;
+
+public static partial class TypeF
+{
+	/// <summary>
+	/// Cache value types as they are looked up a lot
+	/// </summary>
+	internal static IMemoryCache StrongIdValueTypes { get; } = new MemoryCache(new MemoryCacheOptions());
+
+	/// <summary>
+	/// Get the <see cref="IStrongId{T}"/> Value type if <paramref name="type"/>
+	/// implements <see cref="IStrongId{T}"/>
+	/// </summary>
+	/// <param name="type">Type to check</param>
+	public static Type? GetStrongIdValueType(Type type) =>
+		StrongIdValueTypes.GetOrCreate(
+			type.FullName ?? type.Name,
+			_ =>
+			{
+				// Get    .. all interfaces implemented by the type we are checking
+				// If     .. the interface is a generic type (i.e. has generic type arguments)
+				//        .. and the generic type definition is IStrongId<>
+				// Select .. the first (and only) generic type argument - this is the IStrongId<T> Value type
+				var valueTypes = from i in type.GetInterfaces()
+								 where i.IsGenericType
+								 && i.GetGenericTypeDefinition() == typeof(IStrongId<>)
+								 select i.GenericTypeArguments[0];
+
+				// If precisely one value type has been identified, return it
+				if (valueTypes.Count() == 1)
+				{
+					return valueTypes.Single();
+				}
+
+				// This means the type doesn't implement IStrongId<T>,
+				// or it implements it with multiple Value types, which is not supported
+				return null;
+			}
+		);
+}
---
+++ src/StrongId/Functions/TypeF.cs
@@ -0,0 +1,9 @@
+// Mileage Tracker
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
+
+namespace StrongId.Functions;
+
+/// <summary>
+/// Type functions
+/// </summary>
+public static partial class TypeF { }
