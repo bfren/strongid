2695dbe5bee9eeeb9731d507af64fdc42c240fc2
Ben Green <ben@bcgdesign.com>
2022-03-31 10:55:25 +0100

    Moving generic Value implementation to IStrongId - #13

--- src/StrongId/IStrongId.cs
+++ src/StrongId/IStrongId.cs
@@ -1,6 +1,8 @@
 // StrongId: Strongly-Typed ID Values
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
 
+using System;
+
 namespace StrongId;
 
 /// <summary>
@@ -25,4 +27,21 @@ internal interface IStrongId<T> : IStrongId
 {
 	/// <inheritdoc cref="IStrongId.Value"/>
 	new T Value { get; init; }
+
+	/// <summary>
+	/// Implement <see cref="IStrongId.Value"/> explicitly to enable the type-specific override
+	/// </summary>
+	/// <exception cref="InvalidCastException"></exception>
+	object IStrongId.Value
+	{
+		get => Value ?? new object();
+		init => Value = value switch
+		{
+			T v =>
+				v,
+
+			_ =>
+				throw new InvalidCastException($"Unable to set ID value to {value}: expecting an object of type {typeof(T)}.")
+		};
+	}
 }
