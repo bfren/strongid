5ce639b1de0c832a1f8f9c253c2e42408a1761c7
bfren <ben@bcgdesign.com>
2022-04-01 11:02:08 +0100

    Moving random generators to Generator class - #19

--- src/StrongId/IntId.cs
+++ src/StrongId/IntId.cs
@@ -1,8 +1,6 @@
 // StrongId: Strongly-Typed ID Values
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
 
-using RndF;
-
 namespace StrongId;
 
 /// <summary>
@@ -20,12 +18,4 @@ public abstract record class IntId : StrongId<int>
 	/// </summary>
 	/// <param name="value">ID Value</param>
 	protected IntId(int value) : base(value) { }
-
-	/// <summary>
-	/// Generate a new <typeparamref name="TId"/> with a random <see cref="IStrongId.Value"/>
-	/// </summary>
-	/// <typeparam name="TId"><see cref="IStrongId"/> type</typeparam>
-	public static TId RndId<TId>()
-		where TId : StrongId<int>, new() =>
-		new() { Value = Rnd.NumberF.GetInt32() };
 }
