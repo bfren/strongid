d60bf6f58da3efed894bda4ac0d37babec3f8801
bfren <ben@bcgdesign.com>
2022-04-06 08:45:52 +0100

    Adding use cache option - #25

--- src/StrongId/Functions/TypeF.GetStrongIdValueType.cs
+++ src/StrongId/Functions/TypeF.GetStrongIdValueType.cs
@@ -3,12 +3,18 @@
 
 using System;
 using System.Linq;
+using System.Threading;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace StrongId.Functions;
 
 public static partial class TypeF
 {
+	/// <summary>
+	/// Enables caching of value types to avoid repetitive reflection
+	/// </summary>
+	public static bool EnableValueTypeCache { get; set; } = true;
+
 	/// <summary>
 	/// Cache value types as they are looked up a lot
 	/// </summary>
@@ -20,35 +26,63 @@ public static partial class TypeF
 	/// </summary>
 	/// <param name="type">Type to check</param>
 	public static Type? GetStrongIdValueType(Type type) =>
-		StrongIdValueTypes.GetOrCreate(
-			type.FullName ?? type.Name,
-			_ =>
-			{
-				// Strong IDs must implement IStrongId as a minimum
-				if (!typeof(IStrongId).IsAssignableFrom(type))
-				{
-					return null;
-				}
+		EnableValueTypeCache switch
+		{
+			true =>
+				GetOrCreateCacheEntry(StrongIdValueTypes, type),
 
-				// Get    .. all interfaces implemented by the type we are checking
-				// If     .. the interface is a generic type (i.e. has generic type arguments)
-				//        .. and the generic type definition is IStrongId<>
-				// Select .. the first (and only) generic type argument - this is the IStrongId<T> Value type
-				var valueTypesQuery = from i in type.GetInterfaces()
-									  where i.IsGenericType
-									  && i.GetGenericTypeDefinition() == typeof(IStrongId<>)
-									  select i.GenericTypeArguments[0];
-				var valueTypes = valueTypesQuery.ToList();
+			false =>
+				GetValueTypeOrNull(type)
+		};
 
-				// If the count is not 1, this means the type doesn't implement IStrongId<T>,
-				// or it implements it multiple times, which is not supported
-				if (valueTypes.Count != 1)
-				{
-					return null;
-				}
+	private static readonly SemaphoreSlim CacheLock = new(1, 1);
 
-				// Precisely one value type has been identified, so return it
-				return valueTypes[0];
-			}
-		);
+	/// <inheritdoc cref="GetStrongIdValueType(Type)"/>
+	internal static Type? GetOrCreateCacheEntry(IMemoryCache cache, Type type)
+	{
+		if (cache.TryGetValue(type.GetHashCode(), out Type? t))
+		{
+			return t;
+		}
+
+		CacheLock.Wait();
+		try
+		{
+			return cache.GetOrCreate(type.GetHashCode(), _ => GetValueTypeOrNull(type));
+		}
+		finally
+		{
+			_ = CacheLock.Release();
+		}
+	}
+
+	/// <inheritdoc cref="GetStrongIdValueType(Type)"/>
+	internal static Type? GetValueTypeOrNull(Type type)
+	{
+		// Strong IDs must implement IStrongId as a minimum
+		if (!typeof(IStrongId).IsAssignableFrom(type))
+		{
+			return null;
+		}
+
+		// Get    .. all interfaces implemented by the type we are checking
+		// If     .. the interface is a generic type (i.e. has generic type arguments)
+		//        .. and the generic type definition is IStrongId<>
+		// Select .. the first (and only) generic type argument - this is the IStrongId<T> Value type
+		var valueTypesQuery = from i in type.GetInterfaces()
+							  where i.IsGenericType
+							  && i.GetGenericTypeDefinition() == typeof(IStrongId<>)
+							  select i.GenericTypeArguments[0];
+		var valueTypes = valueTypesQuery.ToList();
+
+		// If the count is not 1, this means the type doesn't implement IStrongId<T>,
+		// or it implements it multiple times, which is not supported
+		if (valueTypes.Count != 1)
+		{
+			return null;
+		}
+
+		// Precisely one value type has been identified, so return it
+		return valueTypes[0];
+	}
 }
