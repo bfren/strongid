d0154dbf6b0465e8f4909cc02d116bca5a1a1a63
bfren <ben@bcgdesign.com>
2022-05-03 09:10:37 +0100

    Testing all model binder types - #38

--- tests/Tests.StrongId.Mvc/ModelBinding/StrongIdModelBinder/BindModelAsync_Tests.cs
+++ tests/Tests.StrongId.Mvc/ModelBinding/StrongIdModelBinder/BindModelAsync_Tests.cs
@@ -7,7 +7,8 @@ namespace StrongId.Mvc.ModelBinding.StrongIdModelBinder_Tests;
 
 public class BindModelAsync_Tests
 {
-	internal static (StrongIdModelBinder<TestLongId, long>, Vars) Setup()
+	internal static (StrongIdModelBinder<TId, TIdValue>, Vars<TIdValue>) Setup<TId, TIdValue>()
+		where TId : StrongId<TIdValue>, new()
 	{
 		var modelName = Rnd.Str;
 		var modelState = new ModelStateDictionary();
@@ -15,47 +16,40 @@ public class BindModelAsync_Tests
 		var valueResult = new ValueProviderResult(modelValue);
 
 		var valueProvider = Substitute.For<IValueProvider>();
-		valueProvider.GetValue(modelName)
-			.Returns(valueResult);
+		valueProvider.GetValue(modelName).Returns(valueResult);
 
 		var bindingResult = new ModelBindingResult();
 
 		var bindingContext = Substitute.ForPartsOf<ModelBindingContext>();
-		bindingContext.ModelName
-			.Returns(modelName);
-		bindingContext.Result
-			.Returns(bindingResult);
-		bindingContext.ModelState
-			.Returns(modelState);
-		bindingContext.ValueProvider
-			.Returns(valueProvider);
+		bindingContext.ModelName.Returns(modelName);
+		bindingContext.Result.Returns(bindingResult);
+		bindingContext.ModelState.Returns(modelState);
+		bindingContext.ValueProvider.Returns(valueProvider);
 
-		var parse = Substitute.For<Func<string?, Maybe<long>>>();
-		parse.Invoke(default)
-			.ReturnsForAnyArgs(Create.None<long>());
+		var parse = Substitute.For<Func<string?, Maybe<TIdValue>>>();
+		parse.Invoke(default).ReturnsForAnyArgs(Create.None<TIdValue>());
 
-		var binder = Substitute.For<StrongIdModelBinder<TestLongId, long>>();
-		binder.Parse(default)
-			.ReturnsForAnyArgs(x => parse(x[0].ToString()));
+		var binder = Substitute.For<StrongIdModelBinder<TId, TIdValue>>();
+		binder.Parse(default).ReturnsForAnyArgs(x => parse(x[0].ToString()));
 
 		return (binder, new(bindingContext, bindingResult, modelName, modelState, modelValue, parse, valueProvider));
 	}
 
-	public sealed record class Vars(
+	public sealed record class Vars<TIdValue>(
 		ModelBindingContext BindingContext,
 		ModelBindingResult BindingResult,
 		string ModelName,
 		ModelStateDictionary ModelState,
 		string ModelValue,
-		Func<string?, Maybe<long>> Parse,
+		Func<string?, Maybe<TIdValue>> Parse,
 		IValueProvider ValueProvider
 	);
 
-	[Fact]
-	public async Task ValueProvider_Result_Is_None__Does_Not_Change_BindingResult()
+	private async Task ValueProvider_Result_Is_None__Does_Not_Change_BindingResult<TId, TIdValue>()
+		where TId : StrongId<TIdValue>, new()
 	{
 		// Arrange
-		var (binder, v) = Setup();
+		var (binder, v) = Setup<TId, TIdValue>();
 		v.ValueProvider.GetValue(v.ModelName)
 			.Returns(ValueProviderResult.None);
 
@@ -67,10 +61,22 @@ public class BindModelAsync_Tests
 	}
 
 	[Fact]
-	public async Task ValueProvider_Result_Is_Not_None__Sets_Model_Value_Using_Result_Value()
+	public async Task Guid__ValueProvider_Result_Is_None__Does_Not_Change_BindingResult() =>
+		await ValueProvider_Result_Is_None__Does_Not_Change_BindingResult<TestGuidId, Guid>();
+
+	[Fact]
+	public async Task Int__ValueProvider_Result_Is_None__Does_Not_Change_BindingResult() =>
+		await ValueProvider_Result_Is_None__Does_Not_Change_BindingResult<TestIntId, int>();
+
+	[Fact]
+	public async Task Long__ValueProvider_Result_Is_None__Does_Not_Change_BindingResult() =>
+		await ValueProvider_Result_Is_None__Does_Not_Change_BindingResult<TestLongId, long>();
+
+	private async Task ValueProvider_Result_Is_Not_None__Sets_Model_Value_Using_Result_Value<TId, TIdValue>()
+		where TId : StrongId<TIdValue>, new()
 	{
 		// Arrange
-		var (binder, v) = Setup();
+		var (binder, v) = Setup<TId, TIdValue>();
 
 		// Act
 		await binder.BindModelAsync(v.BindingContext);
@@ -80,10 +86,22 @@ public class BindModelAsync_Tests
 	}
 
 	[Fact]
-	public async Task ValueProvider_Result_Is_Not_None__Calls_Parse__With_Correct_Value()
+	public async Task Guid__ValueProvider_Result_Is_Not_None__Sets_Model_Value_Using_Result_Value() =>
+		await ValueProvider_Result_Is_Not_None__Sets_Model_Value_Using_Result_Value<TestGuidId, Guid>();
+
+	[Fact]
+	public async Task Int__ValueProvider_Result_Is_Not_None__Sets_Model_Value_Using_Result_Value() =>
+		await ValueProvider_Result_Is_Not_None__Sets_Model_Value_Using_Result_Value<TestIntId, int>();
+
+	[Fact]
+	public async Task Long__ValueProvider_Result_Is_Not_None__Sets_Model_Value_Using_Result_Value() =>
+		await ValueProvider_Result_Is_Not_None__Sets_Model_Value_Using_Result_Value<TestLongId, long>();
+
+	private async Task ValueProvider_Result_Is_Not_None__Calls_Parse__With_Correct_Value<TId, TIdValue>()
+		where TId : StrongId<TIdValue>, new()
 	{
 		// Arrange
-		var (binder, v) = Setup();
+		var (binder, v) = Setup<TId, TIdValue>();
 
 		// Act
 		await binder.BindModelAsync(v.BindingContext);
@@ -93,26 +111,49 @@ public class BindModelAsync_Tests
 	}
 
 	[Fact]
-	public async Task ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_None__Returns_Result_Success_With_Default()
+	public async Task Guid__ValueProvider_Result_Is_Not_None__Calls_Parse__With_Correct_Value() =>
+		await ValueProvider_Result_Is_Not_None__Calls_Parse__With_Correct_Value<TestGuidId, Guid>();
+
+	[Fact]
+	public async Task Int__ValueProvider_Result_Is_Not_None__Calls_Parse__With_Correct_Value() =>
+		await ValueProvider_Result_Is_Not_None__Calls_Parse__With_Correct_Value<TestIntId, int>();
+
+	[Fact]
+	public async Task Long__ValueProvider_Result_Is_Not_None__Calls_Parse__With_Correct_Value() =>
+		await ValueProvider_Result_Is_Not_None__Calls_Parse__With_Correct_Value<TestLongId, long>();
+
+	private async Task ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_None__Returns_Result_Success_With_Default<TId, TIdValue>()
+		where TId : StrongId<TIdValue>, new()
 	{
 		// Arrange
-		var (binder, v) = Setup();
+		var (binder, v) = Setup<TId, TIdValue>();
 
 		// Act
 		await binder.BindModelAsync(v.BindingContext);
 
 		// Assert
 		Assert.True(v.BindingContext.Result.IsModelSet);
-		var model = Assert.IsType<TestLongId>(v.BindingContext.Result.Model);
+		var model = Assert.IsType<TId>(v.BindingContext.Result.Model);
 		Assert.Equal(binder.Default, model.Value);
 	}
 
 	[Fact]
-	public async Task ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_Some__Returns_Result_Success_With_Value()
+	public async Task Guid__ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_None__Returns_Result_Success_With_Default() =>
+		await ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_None__Returns_Result_Success_With_Default<TestGuidId, Guid>();
+
+	[Fact]
+	public async Task Int__ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_None__Returns_Result_Success_With_Default() =>
+		await ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_None__Returns_Result_Success_With_Default<TestIntId, int>();
+
+	[Fact]
+	public async Task Long__ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_None__Returns_Result_Success_With_Default() =>
+		await ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_None__Returns_Result_Success_With_Default<TestLongId, long>();
+
+	private async Task ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_Some__Returns_Result_Success_With_Value<TId, TIdValue>(TIdValue id)
+		where TId : StrongId<TIdValue>, new()
 	{
 		// Arrange
-		var (binder, v) = Setup();
-		var id = Rnd.Lng;
+		var (binder, v) = Setup<TId, TIdValue>();
 		v.Parse(v.ModelValue)
 			.Returns(id);
 
@@ -121,9 +162,25 @@ public class BindModelAsync_Tests
 
 		// Assert
 		Assert.True(v.BindingContext.Result.IsModelSet);
-		var model = Assert.IsType<TestLongId>(v.BindingContext.Result.Model);
+		var model = Assert.IsType<TId>(v.BindingContext.Result.Model);
 		Assert.Equal(id, model.Value);
 	}
+
+	[Fact]
+	public async Task Guid__ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_Some__Returns_Result_Success_With_Value() =>
+		await ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_Some__Returns_Result_Success_With_Value<TestGuidId, Guid>(Rnd.Guid);
+
+	[Fact]
+	public async Task Int__ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_Some__Returns_Result_Success_With_Value() =>
+		await ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_Some__Returns_Result_Success_With_Value<TestIntId, int>(Rnd.Int);
+
+	[Fact]
+	public async Task Long__ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_Some__Returns_Result_Success_With_Value() =>
+		await ValueProvider_Result_Is_Not_None__Calls_Parse__Receives_Some__Returns_Result_Success_With_Value<TestLongId, long>(Rnd.Lng);
+
+	public sealed record class TestGuidId : GuidId;
+
+	public sealed record class TestIntId : IntId;
 
 	public sealed record class TestLongId : LongId;
 }
