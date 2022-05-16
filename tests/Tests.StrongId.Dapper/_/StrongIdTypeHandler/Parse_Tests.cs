8ca75d264de10622a1e358831998cb6a3aef69ed
bfren <ben@bcgdesign.com>
2022-05-16 09:52:54 +0100

    Adding UIntId and ULongId Dapper type support - #49

--- tests/Tests.StrongId.Dapper/_/StrongIdTypeHandler/Parse_Tests.cs
+++ tests/Tests.StrongId.Dapper/_/StrongIdTypeHandler/Parse_Tests.cs
@@ -75,6 +75,7 @@ public class Parse_Tests
 
 	[Theory]
 	[InlineData(42, 42)]
+	[InlineData(42.0, 42)]
 	[InlineData("42", 42)]
 	[InlineData(" 42 ", 42)]
 	public void Valid_Input__Returns_IntId(object input, int expected)
@@ -107,6 +108,43 @@ public class Parse_Tests
 		Assert.Equal(expected, result.Value);
 	}
 
+	[Theory]
+	[InlineData(42, 42u)]
+	[InlineData(42u, 42u)]
+	[InlineData(42.0, 42u)]
+	[InlineData("42", 42u)]
+	[InlineData(" 42 ", 42u)]
+	public void Valid_Input__Returns_UIntId(object input, uint expected)
+	{
+		// Arrange
+		var handler = new StrongIdTypeHandler<TestUIntId>();
+
+		// Act
+		var result = handler.Parse(input);
+
+		// Assert
+		Assert.Equal(expected, result.Value);
+	}
+
+	[Theory]
+	[InlineData(42, 42UL)]
+	[InlineData(42L, 42UL)]
+	[InlineData(42UL, 42UL)]
+	[InlineData(42.0, 42UL)]
+	[InlineData("42", 42UL)]
+	[InlineData(" 42 ", 42UL)]
+	public void Valid_Input__Returns_ULongId(object input, ulong expected)
+	{
+		// Arrange
+		var handler = new StrongIdTypeHandler<TestULongId>();
+
+		// Act
+		var result = handler.Parse(input);
+
+		// Assert
+		Assert.Equal(expected, result.Value);
+	}
+
 	[Fact]
 	public void Invalid_Value_Type__Throws_InvalidOperationException()
 	{
@@ -125,6 +163,10 @@ public class Parse_Tests
 	public sealed record class TestIntId : IntId;
 
 	public sealed record class TestLongId : LongId;
+
+	public sealed record class TestUIntId : UIntId;
+
+	public sealed record class TestULongId : ULongId;
 
 	public sealed record class InvalidObjectId(object Value) : IStrongId
 	{
